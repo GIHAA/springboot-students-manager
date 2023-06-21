09e2be571832fe3eec41b31d12352c9329a5d59f
GIHAA <gihansudeeparandimal@gmail.com>
2023-06-21 19:56:37 +0530

    Connection to pg db added

--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -1,11 +1,10 @@
 package com.example.demo.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDate;
-import java.time.Month;
 import java.util.List;
 
 @RestController
@@ -13,17 +12,17 @@ import java.util.List;
 public class StudentController {
 
 
+    private final StudentService studentService;
+
+    @Autowired
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
+
     @GetMapping
     public List<Student> getStudents(){
-        return List.of(
-                new Student(
-                        1L,
-                        "Mariam",
-                        "adasd",
-                        LocalDate.of(2000 , Month.FEBRUARY , 5),
-                        21
-                )
-        );
+        return  studentService.getStudents();
     }
 
 }
